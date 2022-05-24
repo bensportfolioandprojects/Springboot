485175b48863785c20a020af28035a1c250ce4f9
Ben <benphillips96@outlook.com>
2022-05-24 15:32:52 +0100

    after adding service into API

--- src/main/java/com/qa/SpringbootEx/controllers/UserController.java
+++ src/main/java/com/qa/SpringbootEx/controllers/UserController.java
@@ -1,44 +1,60 @@
 package com.qa.SpringbootEx.controllers;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.SpringbootEx.Domain.User;
+import com.qa.SpringbootEx.services.UserService;
 
 @RestController
 public class UserController {
 	
-	private List<User> users = new ArrayList<>();
-
+	private UserService service;
 	
-////	Read - Get Request
-//	@GetMapping("/get")
-//	public String hello() {
-//		return "Hello";
-//	}
-	
-//	Read - Get Request
-	@GetMapping("/getAll")
-	public List<User> getAll() {
-		return users;
+	public UserController(UserService service) {
+		this.service = service;
 	}
 	
-//	Create - Post
+	//	Read - Get Request
+	@GetMapping("/getAll")
+	public List<User> getAll() {
+		return service.getAll();
+	
+	}
+	
+	//	Read All
+	@GetMapping("/getById/{id}")
+	public User getById(@PathVariable long id) {
+		return service.getById(id);
+		
+	}
+	
+	//	Create - Post
 	@PostMapping("/create")
 	public User create (@RequestBody User user) {
-		users.add(user);
-		return users.get(users.size() -1);
+		return service.create(user);
+		
     }
 	
-//	Update - Put/Patch
+	//	Update - Put/Patch
+	@PutMapping("/update/{id}")
+	public User update(@PathVariable long id, @RequestBody User user) {
+		return service.update(id, user);
+	}
 	
 	
-//	Delete - Delete Request
+	//	Delete - Delete Request
+	@DeleteMapping("/delete/{id}")
+	public User delete(@PathVariable long id) {
+		return service.delete(id);
+	}
 	
 
 }
---
+++ src/main/java/com/qa/SpringbootEx/services/UserService.java
@@ -0,0 +1,40 @@
+package com.qa.SpringbootEx.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import com.qa.SpringbootEx.Domain.User;
+
+@Service
+public class UserService {
+	
+	// Temporary storage, until we implement the real database.
+		private List<User> users = new ArrayList<>();
+		
+		public List<User> getAll() {
+			return users;
+		}
+		
+		public User getById(long id) {
+			return users.get((int)id);
+		}
+		
+		public User create(User user) {
+			users.add(user);
+			return users.get(users.size() - 1);
+		}
+		
+		public User update(long id, User user) {
+			// Remove the original user
+			users.remove((int)id);
+			// Add the updated user
+			users.add((int)id, user);
+			// Return the updated user
+			return users.get((int)id);
+		}
+
+		public User delete(long id) {
+			return users.remove((int)id);
+		}	
+	}
